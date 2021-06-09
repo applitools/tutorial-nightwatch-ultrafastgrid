365ad7da7d1cfd075a1bbaaae32e15f5bcf23221
Colby Fayock <colbyfayock@users.noreply.github.com>
2021-06-09 13:57:48 -0400

    Update example-ultrafastgrid.js

--- test/example-ultrafastgrid.js
+++ test/example-ultrafastgrid.js
@@ -3,7 +3,7 @@ module.exports = {
     'Ultrafast Test': function(browser) {
         browser
             .url('https://demo.applitools.com')
-            .eyesOpen('Demo App - nightwatch', 'Smoke Test', {width: 800, height: 600})
+            .eyesOpen('Demo App - Nightwatch', 'Smoke Test - Nightwatch', {width: 800, height: 600})
             .eyesCheck(Target.window().fully().withName('Login Window'))
             .click('#log-in')
             .eyesCheck(Target.window().fully().withName('App Window'))
